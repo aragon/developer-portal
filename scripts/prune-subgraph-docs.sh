6ce8382a7eceb40e265e04faa67f0fbf807a4d98
banasa44 <cfbennassar@gmail.com>
2024-04-16 15:12:35 +0200

    fix: add pagination_next/prev to skip sidebar hidden pages in pagination too

--- scripts/prune-subgraph-docs.sh
+++ scripts/prune-subgraph-docs.sh
@@ -18,3 +18,21 @@ fi
 if ! line_exists "sidebar_class_name: hidden" "docs/subgraph/reference-guide/objects/query.mdx"; then
     sed -i '2s/^/sidebar_class_name: hidden\n/' "docs/subgraph/reference-guide/objects/query.mdx"
 fi
+
+# Add pagination_next/pagination_previous to gracefully skip the previous sidbar hidden pages, to hide them from the
+# nex/previous page buttons at the bottom of the webpages
+if ! line_exists "pagination_prev: subgraph/reference-guide/index" "docs/subgraph/reference-guide/enums/permission-operation.mdx"; then
+    sed -i '2s#^#pagination_prev: subgraph/reference-guide/index\n#' "docs/subgraph/reference-guide/enums/permission-operation.mdx"
+fi
+
+if ! line_exists "pagination_next: subgraph/reference-guide/objects/standard-callback" "docs/subgraph/reference-guide/objects/plugin-version.mdx"; then
+    sed -i '2s#^#pagination_next: subgraph/reference-guide/objects/standard-callback\n#' "docs/subgraph/reference-guide/objects/plugin-version.mdx"
+fi
+
+if ! line_exists "pagination_prev: subgraph/reference-guide/objects/plugin-version" "docs/subgraph/reference-guide/objects/standard-callback.mdx"; then
+    sed -i '2s#^#pagination_prev: subgraph/reference-guide/objects/plugin-version\n#' "docs/subgraph/reference-guide/objects/standard-callback.mdx"
+fi
+
+if ! line_exists "pagination_next: null" "docs/subgraph/reference-guide/objects/transaction-actions-proposal.mdx"; then
+    sed -i '2s/^/pagination_next: null\n/' "docs/subgraph/reference-guide/objects/transaction-actions-proposal.mdx"
+fi
