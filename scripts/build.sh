8a21c8a274acf8334d0443f10093a1f645060d6d
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-12-04 20:42:53 +0400

    new push

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,15 +6,6 @@ log() {
   echo "$*" >&2
 }
 
-if [ -n "${NETLIFY:+x}" ]; then
-  : ${NETLIFY_BUILD_BASE="/opt/build"}
-  NETLIFY_CACHE_DIR="$NETLIFY_BUILD_BASE/cache"
-  export XDG_CACHE_HOME="$NETLIFY_CACHE_DIR/xdg"
-
-  if [ "$CONTEXT" != "production" ]; then
-    export URL="$DEPLOY_PRIME_URL"
-  fi
-fi
 
 ## Antora
 
